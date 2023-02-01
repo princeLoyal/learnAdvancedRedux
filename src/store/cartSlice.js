fd080b7f0f457b9275d6b677a998bdeee23399b3
princeLoyal <princeloyal1234@gmail.com>
2023-02-01 18:28:38 +0100

    update

--- src/store/cartSlice.js
+++ src/store/cartSlice.js
@@ -10,6 +10,7 @@ import { createSlice } from '@reduxjs/toolkit';
         addItemToCart(state, action){
             const newItem = action.payload;
             const existingItem = state.items.find(item => item.id === newItem.id);
+            state.totalQuantity++;
             if(!existingItem){
                 state.items.push({
                     itemId: newItem.id, 
@@ -26,11 +27,12 @@ import { createSlice } from '@reduxjs/toolkit';
         removeItemFromCart(state, action){
             const id = action.payload;
             const existingItem = state.items.find(item => item.id === id);
+            state.totalQuantity--;
             if(existingItem.quantity === 1){
                 state.items = state.items.filter(item => item.id !== id);
             } else {
                 existingItem.quantity--;
-                existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
+                // existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
             }
         }
     }
