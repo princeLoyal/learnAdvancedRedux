9d6ba5473a5dde0019d6bf80f5a327d559cd5d00
princeLoyal <98643554+princeLoyal@users.noreply.github.com>
2023-02-02 12:08:06 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -17,8 +17,8 @@ function App() {
 
 dispatch(uiActions.setNotification({
   status:'pending',
-  title: 'pending!',
-  message: 'Sending Cart to database'
+  title: 'Sending...',
+  message: 'Sending cart data!'
 }));
     const fetchHandler = async() => {
       const response = await fetch('https://react-http-c3510-default-rtdb.firebaseio.com/cart.json', {
