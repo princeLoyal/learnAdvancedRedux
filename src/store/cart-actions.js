140d4986f04d9de76bbaccd3e937289d87705a1e
princeLoyal <98643554+princeLoyal@users.noreply.github.com>
2023-02-05 00:18:46 +0100

    Update cart-actions.js

--- src/store/cart-actions.js
+++ src/store/cart-actions.js
@@ -38,20 +38,32 @@ export const sendCartData = (cart) => {
 
 export const getCartData = () => {
     return async (dispatch) => {
+       dispatch(uiActions.setNotification({
+            status:'Ongoing',
+            title: 'Retrieving...',
+            message: 'Getting cart data!'
+        }));
         try {
         const getRequest = async () => {
             const response = await fetch('https://react-http-c3510-default-rtdb.firebaseio.com/cart.json');
             const data = await response.json();
-alert(data.totalQuantity)
-alert(data.items)
             dispatch(cartActions.replaceCart({
                totalQuantity: data.totalQuantity,
                items: data.items
             }));
         }
-getRequest();
+          await getRequest();
+          dispatch(uiActions.setNotification({
+                status:'success',
+                title: 'Success!',
+                message: 'Got Cart data succesfully'
+              }));
         } catch(error) {
-
+          dispatch(uiActions.setNotification({
+                status:'error',
+                title: 'Error!',
+                message: 'Retrieving Cart data failed.'
+              }));
         }
     };
 };
