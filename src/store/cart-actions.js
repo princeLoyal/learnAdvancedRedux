a76890757697d3f14171f4832335d1bb2a77a5ea
princeLoyal <98643554+princeLoyal@users.noreply.github.com>
2023-02-05 00:08:59 +0100

    Update cart-actions.js

--- src/store/cart-actions.js
+++ src/store/cart-actions.js
@@ -42,6 +42,8 @@ export const getCartData = () => {
         const getRequest = async () => {
             const response = await fetch('https://react-http-c3510-default-rtdb.firebaseio.com/cart.json');
             const data = await response.json();
+alert(data.totalQuantity)
+alert(data.items)
             dispatch(cartActions.replaceCart({
                totalQuantity: data.totalQuantity,
                items: data.items
